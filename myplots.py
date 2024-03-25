7ae8d78daa0d2cc988761b1f97d2a465fc04e20e
Ganesh <71372490+gana36@users.noreply.github.com>
2024-03-25 16:09:53 +0000

    part1.py

--- myplots.py
+++ myplots.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import utils as u
-
 #----------------------------------------------------------------------
 def plot_part1C(kmeans_dct, file_nm):
     """
@@ -8,20 +7,16 @@ def plot_part1C(kmeans_dct, file_nm):
     Plot the datasets in kmeans_dct . 
     Each row: value of k
     Each column: value a dataset
-
     Aguments: 
     kmeans_dct: kmeans_dct.keys(): (
     """
-    nb_ks = 4
+    nb_ks = 4 #4
     nb_datasets = len(kmeans_dct.keys())
     print(f"{kmeans_dct=}")
     print(f"{nb_datasets=}")
     print(f"{nb_ks=}")
-
     figure, axes = plt.subplots(nb_ks, nb_datasets, figsize=(10, 10))
-
     data_dict = {}
-
     for i, (key, v) in enumerate(kmeans_dct.items()):
         X, y = v[0]
         y_kmeans = v[1]
@@ -31,9 +26,7 @@ def plot_part1C(kmeans_dct, file_nm):
             ax.set_title(f"{key}, k={k}")
             ax.set_xlabel("x")
             ax.set_ylabel("y")
-
     plt.tight_layout()
     # plt.show()
     plt.savefig(file_nm)
-
     return 
--- part3.py
+++ part3.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 import scipy.io as io
 from scipy.cluster.hierarchy import dendrogram, linkage  #
+from scipy.spatial.distance import pdist
 
 # import plotly.figure_factory as ff
 import math
@@ -27,8 +28,20 @@ Recall from lecture that agglomerative hierarchical clustering is a greedy itera
 # the question asked.
 
 
-def data_index_function():
-    return None
+def data_index_function(data,I,J):
+    indices=[]
+    for i in I:
+        indices.append(data[i])
+    for j in J:
+        indices.append(data[j])
+    distance_matrix = pdist(data, metric='euclidean')
+
+    Z = linkage(distance_matrix, method='single')
+
+    dissimilarities = Z[:, 2]
+    # print(dissimilarities)
+
+    return dissimilarities
 
 
 def compute():
@@ -37,32 +50,38 @@ def compute():
     """
     A.	Load the provided dataset “hierachal_toy_data.mat” using the scipy.io.loadmat function.
     """
-
+    data=io.loadmat("hierarchical_toy_data.mat")
     # return value of scipy.io.loadmat()
-    answers["3A: toy data"] = {}
+    answers["3A: toy data"] = data
 
     """
     B.	Create a linkage matrix Z, and plot a dendrogram using the scipy.hierarchy.linkage and scipy.hierachy.dendrogram functions, with “single” linkage.
     """
+    Z = linkage(data['X'], 'single')
+    # fig = plt.figure(figsize=(25, 10))
+    dn = dendrogram(Z)
+    # plt.savefig('part3_questionA.png')
 
     # Answer: NDArray
-    answers["3B: linkage"] = np.zeros(1)
-
+    answers["3B: linkage"] = Z
+    # print(Z)
     # Answer: the return value of the dendogram function, dicitonary
-    answers["3B: dendogram"] = {}
+    answers["3B: dendogram"] = dn
+    # print(dn)
 
     """
     C.	Consider the merger of the cluster corresponding to points with index sets {I={8,2,13}} J={1,9}}. At what iteration (starting from 0) were these clusters merged? That is, what row does the merger of A correspond to in the linkage matrix Z? The rows count from 0. 
     """
 
     # Answer type: integer
-    answers["3C: iteration"] = -1
+    answers["3C: iteration"] = 4
 
     """
     D.	Write a function that takes the data and the two index sets {I,J} above, and returns the dissimilarity given by single link clustering using the Euclidian distance metric. The function should output the same value as the 3rd column of the row found in problem 2.C.
     """
     # Answer type: a function defined above
-    answers["3D: function"] = data_index_function
+    answers["3D: function"] = data_index_function(data['X'],I={8,2,13},J={1,9})
+    # print(answers["3D: function"])
 
     """
     E.	In the actual algorithm, deciding which clusters to merge should consider all of the available clusters at each iteration. List all the clusters as index sets, using a list of lists, 
@@ -70,14 +89,14 @@ def compute():
     """
 
     # List the clusters. the [{0,1,2}, {3,4}, {5}, {6}, ...] represents a list of lists.
-    answers["3E: clusters"] = [{0, 0}, {0, 0}]
+    answers["3E: clusters"] = [{4}, {6,14},{8,2,13,1,9},{5},{11},{0},{10},{3},{7},{12}]
 
     """
     F.	Single linked clustering is often criticized as producing clusters where “the rich get richer”, that is, where one cluster is continuously merging with all available points. Does your dendrogram illustrate this phenomenon?
     """
 
     # Answer type: string. Insert your explanation as a string.
-    answers["3F: rich get richer"] = ""
+    answers["3F: rich get richer"] = "Yes, we could see as we move close to origin the merging of clusters seem to behave like one cluster is formed by merging all the points present."
 
     return answers
 
