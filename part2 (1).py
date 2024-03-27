28cb2524d67f9ab03fd899916ca0689dd6198876
Ganesh <71372490+gana36@users.noreply.github.com>
2024-03-27 13:51:23 -0400

    Add files via upload

---
+++ part2 (1).py
@@ -0,0 +1,123 @@
+from pprint import pprint
+
+# import plotly.figure_factory as ff
+import math
+from sklearn.cluster import AgglomerativeClustering
+import pickle
+import utils as u
+
+import myplots as myplt
+import time
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster, datasets, mixture
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+import scipy.io as io
+from scipy.cluster.hierarchy import dendrogram, linkage  #
+
+# import plotly.figure_factory as ff
+import math
+from sklearn.cluster import AgglomerativeClustering,KMeans
+import pickle
+import utils as u
+
+
+# ----------------------------------------------------------------------
+"""
+Part 2
+Comparison of Clustering Evaluation Metrics: 
+In this task you will explore different methods to find a good value 
+for k
+"""
+
+# Fill this function with code at this location. Do NOT move it. 
+# Change the arguments and return according to 
+# the question asked. 
+
+model_sse_inertial={}
+model_sse_manual={}
+def fit_kmeans(X,k):
+    sse_inertia=[]
+    sse_man=[]
+    for clusters_num in range(1,k+1):
+      kmeans=KMeans(n_clusters=clusters_num)
+      preds=kmeans.fit_predict(X)
+      sse={}
+      for iter,prediction in enumerate(preds):
+        alp=(X[iter][0]-kmeans.cluster_centers_[prediction][0])**2 + (X[iter][1]-kmeans.cluster_centers_[prediction][1])**2
+        try:
+          sse[prediction]+=alp
+        except:
+          sse[prediction]=alp
+
+      sse_inertia.append(kmeans.inertia_)
+      sse_manual=0
+      for i in sse:
+        sse_manual+=sse[i]
+      sse_man.append(sse_manual)
+      model_sse_inertial[clusters_num]=kmeans.inertia_
+      model_sse_manual[clusters_num]=sse_manual
+
+    return sse_inertia,sse_man
+
+
+def compute():
+    # ---------------------
+    answers = {}
+
+    """
+    A.	Call the make_blobs function with following parameters :(center_box=(-20,20), n_samples=20, centers=5, random_state=12).
+    """
+    n_samples=20
+    center_box=(-20,20)
+    centers=5
+    random_state=12
+    x,label = datasets.make_blobs(n_samples=n_samples, centers=centers,center_box=center_box, random_state=random_state)
+    cord_1=x[0:,0:1]
+    cord_2=x[0:,1:]
+    # dct: return value from the make_blobs function in sklearn, expressed as a list of three numpy arrays
+    dct = answers["2A: blob"] = [cord_1,cord_2,label] #[np.zeros(0)]
+    """
+    B. Modify the fit_kmeans function to return the SSE (see Equations 8.1 and 8.2 in the book).
+    """
+  
+    X=np.concatenate([answers["2A: blob"][0],answers['2A: blob'][1]],axis=1)
+    # # print(X)
+    # # dct value: the `fit_kmeans` function
+    dct = answers["2B: fit_kmeans"] = fit_kmeans
+
+    # """
+    # C.	Plot the SSE as a function of k for k=1,2,….,8, and choose the optimal k based on the elbow method.
+    # """
+    sse_c=fit_kmeans(X,k=8)[1]
+    sse_vs_k=[[x,y] for x,y in zip(range(1,9),sse_c)]
+  
+    # # dct value: a list of tuples, e.g., [[0, 100.], [1, 200.]]
+    # # Each tuple is a (k, SSE) pair
+
+    dct = answers["2C: SSE plot"] =sse_vs_k#[[0.0, 100.0]]
+
+    # """
+    # D.	Repeat part 2.C for inertia (note this is an attribute in the kmeans estimator called _inertia). Do the optimal k’s agree?
+    # """
+    sse_d=fit_kmeans(X,k=8)[0]
+    sse_vs_k=[[x,y] for x,y in zip(range(1,9),sse_d)]
+    
+    # # dct value has the same structure as in 2C
+    dct = answers["2D: inertia plot"] = sse_vs_k
+    # # dct value should be a string, e.g., "yes" or "no"
+    dct = answers["2D: do ks agree?"] = "no"
+
+    return answers
+
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+    answers = compute()
+
+    with open("part2.pkl", "wb") as f:
+        pickle.dump(answers, f)
---
+++ part4 (1).py
@@ -0,0 +1,147 @@
+import time
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster, datasets, mixture
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+import scipy.io as io
+from scipy.cluster.hierarchy import dendrogram, linkage  #
+
+# import plotly.figure_factory as ff
+import math
+from sklearn.cluster import AgglomerativeClustering
+import pickle
+import utils as u
+import myplots as myplt
+
+"""
+Part 4.	
+Evaluation of Hierarchical Clustering over Diverse Datasets:
+In this task, you will explore hierarchical clustering over different datasets. You will also evaluate different ways to merge 
+clusters and good ways to find the cut-off point for breaking the dendrogram.
+"""
+
+# Fill these two functions with code at this location. Do NOT move it. 
+# Change the arguments and return according to 
+# the question asked. 
+
+
+def fit_hierarchical_cluster(data,linkage_type,num_of_clusters):
+    model = AgglomerativeClustering(n_clusters=num_of_clusters, linkage=linkage_type)
+    ss=StandardScaler()
+    train=ss.fit_transform(data[0])
+    model.fit(train,data[1])
+    labels = model.labels_
+    return labels
+
+def fit_modified(data,linkage_type):
+    Z=linkage(data[0],linkage_type)
+
+    distances=[]
+    for i in range(len(Z)):
+        if i+1!=len(Z):
+            merge_dist=Z[i+1,2] - Z[i,2]
+            distances.append(merge_dist)
+    model = AgglomerativeClustering(n_clusters=None, linkage=linkage_type,distance_threshold=np.max(distances))
+    ss=StandardScaler()
+    train=ss.fit_transform(data[0])
+    model.fit(train,data[1])
+    labels = model.labels_
+    return labels
+
+
+def compute():
+    answers = {}
+
+    """
+    A.	Repeat parts 1.A and 1.B with hierarchical clustering. That is, write a function called fit_hierarchical_cluster 
+    (or something similar) that takes the dataset, the linkage type and the number of clusters, that trains an 
+    AgglomerativeClustering sklearn estimator and returns the label predictions. Apply the same standardization as in part 1.B. 
+    Use the default distance metric (euclidean) and the default linkage (ward).
+    """
+
+    # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
+    # keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
+    dct = answers["4A: datasets"] = {}
+    n_samples = 500
+    seed = 42
+    nc = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=seed)
+    dct['nc']=[nc[0],nc[1]]
+
+    nm = datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=seed)
+    dct['nm']=[nm[0],nm[1]]
+
+    bvv = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=seed)
+    dct['bvv']=[bvv[0],bvv[1]]
+
+    X, y = datasets.make_blobs(n_samples=n_samples, random_state=seed)
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
+    X_aniso = np.dot(X, transformation)
+    add = (X_aniso, y)
+    dct['add']=[add[0],add[1]]
+
+    b = datasets.make_blobs(n_samples=n_samples, random_state=seed)
+    #print(b[0].shape,b[1].shape)
+    dct['b']=[b[0],b[1]]
+    
+
+
+    # dct value:  the `fit_hierarchical_cluster` function
+    dct = answers["4A: fit_hierarchical_cluster"] = fit_hierarchical_cluster
+
+    """
+    B.	Apply your function from 4.A and make a plot similar to 1.C with the four linkage types (single, complete, ward, 
+    centroid: rows in the figure), and use 2 clusters for all runs. Compare the results to problem 1, specifically, 
+    are there any datasets that are now correctly clustered that k-means could not handle?
+
+    Create a pdf of the plots and return in your report. 
+    """
+    hierarchical_dict_plotting={}
+    for dataset_i in answers['4A: datasets'].keys():
+        acc=[]
+        dataset_cluster={}
+        for linkage in ['single','complete','ward','average']:
+            preds=dct((answers['4A: datasets'][dataset_i][0],answers['4A: datasets'][dataset_i][1]),linkage,2)
+            dataset_cluster[linkage]=preds
+        acc.append((answers['4A: datasets'][dataset_i][0],answers['4A: datasets'][dataset_i][1]))
+        acc.append(dataset_cluster)
+        hierarchical_dict_plotting[dataset_i]=acc
+ 
+    myplt.plot_part1C(hierarchical_dict_plotting,'Part4_B.jpg')
+    # dct value: list of dataset abbreviations (see 1.C)
+    dct = answers["4B: cluster successes"] = ["nc","nm"]
+
+    """
+    C.	There are essentially two main ways to find the cut-off point for breaking the diagram: specifying the number of clusters and specifying a maximum distance. The latter is challenging to optimize for without knowing and/or directly visualizing the dendrogram, however, sometimes simple heuristics can work well. The main idea is that since the merging of big clusters usually happens when distances increase, we can assume that a large distance change between clusters means that they should stay distinct. Modify the function from part 1.A to calculate a cut-off distance before classification. Specifically, estimate the cut-off distance as the maximum rate of change of the distance between successive cluster merges (you can use the scipy.hierarchy.linkage function to calculate the linkage matrix with distances). Apply this technique to all the datasets and make a plot similar to part 4.B.
+    
+    Create a pdf of the plots and return in your report. 
+    """
+
+    # dct is the function described above in 4.C
+    dct = answers["4A: modified function"] = fit_modified
+    hierarchical_dict_plotting={}
+    for dataset_i in answers['4A: datasets'].keys():
+        acc=[]
+        dataset_cluster={}
+        for linkage in ['single','complete','ward','average']:
+            #x_y=answers['1A: datasets'][dataset_i]
+            preds=dct((answers['4A: datasets'][dataset_i][0],answers['4A: datasets'][dataset_i][1]),linkage)
+            dataset_cluster[linkage]=preds
+        acc.append((answers['4A: datasets'][dataset_i][0],answers['4A: datasets'][dataset_i][1]))
+        acc.append(dataset_cluster)
+        hierarchical_dict_plotting[dataset_i]=acc
+ 
+    myplt.plot_part1C(hierarchical_dict_plotting,'Part4_C.jpg')    
+
+    return answers
+
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+    answers = compute()
+
+    with open("part4.pkl", "wb") as f:
+        pickle.dump(answers, f)
